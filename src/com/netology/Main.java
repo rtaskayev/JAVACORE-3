481a4ff8765b85fa0b4f594cdab44b0ee5cfbe2f
rtaskayev <washingtonqwerty@gmail.com>
2022-09-11 13:13:25 +0200

    JavaCore-3. Files. 3 - DeSerialization and unzipping task

--- src/com/netology/Main.java
+++ src/com/netology/Main.java
@@ -1,10 +1,8 @@
 package com.netology;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.util.zip.ZipInputStream;
 
 public class Main {
 
@@ -13,94 +11,48 @@ public class Main {
         String dirPath = "D:\\Games\\"; // Working directory
         String saveFiles = dirPath + "savegames\\"; // Directory to zip files in
         String zipFile = saveFiles + "zip.zip"; // Output zip file
-        int fileSaveCount = 1; // Count for files naming
-        List<String> filesList; // List of files to archive and delete
 
-        GameProgress gameProgress1 = new GameProgress(60, 20, 3, 4);
-        GameProgress gameProgress2 = new GameProgress(7, 40, 11, 2);
-        GameProgress gameProgress3 = new GameProgress(88, 10, 7, 6);
-
-        // Creating Serialized game progress files
-        saveGame(saveFiles + "save" + fileSaveCount + ".dat", gameProgress1);
-        fileSaveCount++;
-        saveGame(saveFiles + "save" + fileSaveCount + ".dat", gameProgress2);
-        fileSaveCount++;
-        saveGame(saveFiles + "save" + fileSaveCount + ".dat", gameProgress3);
-        fileSaveCount++;
-
-        // Getting list of saved files in directory
-        filesList = getListOfFilesToZip(saveFiles);
-
-        // Archive and delete archived files
-        if (zipFiles(zipFile, filesList)) {
-            deleteArchivedFiles(filesList);
-            System.out.println("Archive and deletion completed");
-        } else {
-            System.out.println("Archive failed");
-        }
+        // Unzip archived files
+        openZip(zipFile, saveFiles);
+        // Print Deserialized object data
+        System.out.println(openProgress("D:\\Games\\savegames\\save1.dat"));
 
     }
 
+    // Method to Deserialize object
+    static GameProgress openProgress(String fileToOpen) {
 
-    // Method to delete archived files
-    static void deleteArchivedFiles(List filesToDelete) {
-        for (int i = 0; i < filesToDelete.size(); i++) {
-            try {
-                File file = new File((String) filesToDelete.get(i));
-                file.delete();
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+        GameProgress gameProgress = null;
+
+        try (FileInputStream fis = new FileInputStream(fileToOpen);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return gameProgress;
+    }
+
+    // Method to unzip files
+    static void openZip(String archiveFilePath, String unzipDirPath) {
+
+        try (ZipInputStream zin = new ZipInputStream(new
+                FileInputStream(archiveFilePath))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = unzipDirPath + entry.getName(); // getting file name
+                // Unzipping
+                FileOutputStream fout = new FileOutputStream(name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
             }
-        }
-    }
-
-    // Method to get list of files to archive
-    static List<String> getListOfFilesToZip(String dirPath) {
-        List<String> filesList = new ArrayList<>();
-        File dir = new File(dirPath);
-
-        for (File item : dir.listFiles()) {
-            if (item.isFile()) {
-                filesList.add(item.getAbsolutePath());
-            }
-        }
-        return filesList;
-    }
-
-    // Method to write game progress
-    static void saveGame(String filePath, GameProgress gameProgress) {
-        try (FileOutputStream fos = new FileOutputStream(filePath);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(gameProgress);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
     }
-
-    // Method to archive files
-    static boolean zipFiles(String archiveFilePath, List filesToArchive) {
-
-            try (FileOutputStream fout = new FileOutputStream(archiveFilePath);
-                 ZipOutputStream zout = new ZipOutputStream(fout)) {
-
-                for (int i = 0; i < filesToArchive.size(); i++) {
-
-                    File fileToZip = new File((String) filesToArchive.get(i));
-                    try (FileInputStream fis = new FileInputStream(fileToZip)) {
-                        ZipEntry entry = new ZipEntry(fileToZip.getName());
-                        zout.putNextEntry(entry);
-                        byte[] buffer = new byte[fis.available()];
-                        fis.read(buffer);
-                        zout.write(buffer);
-                        zout.closeEntry();
-                    } catch (Exception ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                }
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-        return true;
-    }
-
 }
