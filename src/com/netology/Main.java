10ac2ca52e91cb819c6ed05cee2fea6ce0554b2d
rtaskayev <washingtonqwerty@gmail.com>
2022-09-11 12:43:02 +0200

    JavaCore-3. Files. 2 - Serialization and archiving task

--- src/com/netology/Main.java
+++ src/com/netology/Main.java
@@ -1,77 +1,106 @@
 package com.netology;
 
-import javax.imageio.IIOException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Array of directories to be created
-        List<String> dirsList = Arrays.asList("src", "res", "savegames", "temp", "src\\main", "src\\test",
-                "res\\drawables", "res\\vectors", "res\\icons");
-        // Array of files to be created
-        List<String> filesList = Arrays.asList("src\\main\\Main.java", "src\\main\\Utils.java", "temp\\temp.txt");
         String dirPath = "D:\\Games\\"; // Working directory
-        String logFilePath = dirPath + "temp\\temp.txt"; // Log file location
+        String saveFiles = dirPath + "savegames\\"; // Directory to zip files in
+        String zipFile = saveFiles + "zip.zip"; // Output zip file
+        int fileSaveCount = 1; // Count for files naming
+        List<String> filesList; // List of files to archive and delete
 
-        StringBuilder output = new StringBuilder("Starting installation process...\n");
+        GameProgress gameProgress1 = new GameProgress(60, 20, 3, 4);
+        GameProgress gameProgress2 = new GameProgress(7, 40, 11, 2);
+        GameProgress gameProgress3 = new GameProgress(88, 10, 7, 6);
 
-        // Create all directories from Array list
-        for (String s : dirsList) {
-            createDirectory(dirPath + s, output);
-        }
+        // Creating Serialized game progress files
+        saveGame(saveFiles + "save" + fileSaveCount + ".dat", gameProgress1);
+        fileSaveCount++;
+        saveGame(saveFiles + "save" + fileSaveCount + ".dat", gameProgress2);
+        fileSaveCount++;
+        saveGame(saveFiles + "save" + fileSaveCount + ".dat", gameProgress3);
+        fileSaveCount++;
 
-        // Create all files from Array list
-        for (String s : filesList) {
-            createFile(dirPath + s, output);
-        }
+        // Getting list of saved files in directory
+        filesList = getListOfFilesToZip(saveFiles);
 
-        output.append("INSTALLATION COMPLETED!\n");
-        // Log all output to log file
-        logToFile(logFilePath, output);
-        System.out.println("Installation completed. Check " + logFilePath + " for more details");
-
-    }
-
-    // Method to create directory
-    static void createDirectory(String path, StringBuilder output) {
-
-        File pathDir = new File(path);
-
-        if (pathDir.mkdir()) {
-            output.append(path).append(" - directory created\n");
+        // Archive and delete archived files
+        if (zipFiles(zipFile, filesList)) {
+            deleteArchivedFiles(filesList);
+            System.out.println("Archive and deletion completed");
         } else {
-            output.append(path).append(" - ERROR, directory wasn`t created\n");
+            System.out.println("Archive failed");
+        }
+
+    }
+
+
+    // Method to delete archived files
+    static void deleteArchivedFiles(List filesToDelete) {
+        for (int i = 0; i < filesToDelete.size(); i++) {
+            try {
+                File file = new File((String) filesToDelete.get(i));
+                file.delete();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
-    // Method to create file
-    static void createFile(String path, StringBuilder output) {
+    // Method to get list of files to archive
+    static List<String> getListOfFilesToZip(String dirPath) {
+        List<String> filesList = new ArrayList<>();
+        File dir = new File(dirPath);
 
-        File pathDir = new File(path);
-
-        try {
-            if (pathDir.createNewFile())
-                output.append(path).append(" - file created\n");
-        } catch (IOException ex) {
-            output.append(ex.getMessage());
+        for (File item : dir.listFiles()) {
+            if (item.isFile()) {
+                filesList.add(item.getAbsolutePath());
+            }
         }
+        return filesList;
     }
 
-    // Method to put all output to log file
-    static void logToFile(String path, StringBuilder output) {
-
-        try (FileWriter writer = new FileWriter(path)) {
-            writer.write(String.valueOf(output));
-            writer.flush();
-        } catch (IOException ex) {
+    // Method to write game progress
+    static void saveGame(String filePath, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(filePath);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    // Method to archive files
+    static boolean zipFiles(String archiveFilePath, List filesToArchive) {
+
+            try (FileOutputStream fout = new FileOutputStream(archiveFilePath);
+                 ZipOutputStream zout = new ZipOutputStream(fout)) {
+
+                for (int i = 0; i < filesToArchive.size(); i++) {
+
+                    File fileToZip = new File((String) filesToArchive.get(i));
+                    try (FileInputStream fis = new FileInputStream(fileToZip)) {
+                        ZipEntry entry = new ZipEntry(fileToZip.getName());
+                        zout.putNextEntry(entry);
+                        byte[] buffer = new byte[fis.available()];
+                        fis.read(buffer);
+                        zout.write(buffer);
+                        zout.closeEntry();
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        return true;
     }
 
 }
